e7f1ebdd57010c102f136593cb5c7c746d7cec80
jeongHyun-Lee <100923108+jhyun-lee@users.noreply.github.com>
2026-03-12 23:53:12 +0900

    [Bronze II] Title: 분산처리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+def solve():
+
+    t = int(input())
+    
+    for _ in range(t):
+        a, b = map(int, input().split())
+        
+
+        result = pow(a, b, 10)
+        
+        if result == 0:
+            print(10)
+        else:
+            print(result)
+
+solve()
